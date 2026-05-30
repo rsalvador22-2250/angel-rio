4f36b0bfef5f530ac18bd79ad592a98e95e103ed
rsalvador22-2250 <martsalvador2018@gmail.com>
2026-05-30 11:50:24 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import App from './App.jsx'
+
+const CLIENT_ID = '526763192518-r51ls1m258cn6a4brvuasvq8n9c51rah.apps.googleusercontent.com'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>,
+)
